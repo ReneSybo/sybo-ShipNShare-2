37c2f6a32cdb237b0db553e205aff95da9766a50
Zyx <zyxakarene@hotmail.com>
2023-11-28 07:45:53 +0100

    Config setup

---
+++ Assets/Game/Difficulty/DifficultyConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Game.Enemies;
+using UnityEngine;
+
+namespace Game.Difficulty
+{
+	[CreateAssetMenu(menuName = "DifficultyConfig")]
+	public class DifficultyConfig : ScriptableObject
+	{
+		[Header("Enemy related")] 
+		public EnemyComposition[] Compositions = Array.Empty<EnemyComposition>();
+	}
+	
+	[Serializable]
+	public class EnemyComposition
+	{
+		public EnemyConfig Config;
+		public int Count;
+		public int TimeBetweenSpawn;
+	}
+}
